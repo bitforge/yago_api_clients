685f638aec7e3a40343ee75335abf254c630806e
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2021-11-16 11:05:54 +0100

    Generate README for typescript package

--- typescript/generate.sh
+++ typescript/generate.sh
@@ -3,12 +3,13 @@
 # Fetch OpenAPI Schema an generate api client
 npx @openapitools/openapi-generator-cli generate \
     -i http://genie-ar.ch/api/schema/ \
-    -o src/ \
     -g typescript-fetch \
-    --additional-properties=typescriptThreePlus=true
+    --additional-properties=typescriptThreePlus=true \
+    --additional-properties=npmName=genie-api-client
+
 
 # Fix annoying typescript error in generated source
-sed -i -e 's/this\.configuration\.fetchApi/this\.configuration\.fetchApi\!/g' src/runtime.ts
+sed -i '' -e 's/this\.configuration\.fetchApi/this\.configuration\.fetchApi\!/g' src/runtime.ts
 
 CLR='\033[0;32m'
 EOC='\033[0m'
